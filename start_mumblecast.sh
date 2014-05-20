0795fd7d9e77286a5b0040eaa8df49b6006b3e7d
Mumble Server <valexmumble@gmail.com>
2014-05-20 20:40:06 +0000

    Update starting script, add git pull command

--- start_mumblecast.sh
+++ start_mumblecast.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo -Hu www-data git pull
 /etc/init.d/mysql start
 /home/mumblecast/node_modules/forever/bin/forever start /home/mumblecast/server.js
 #../node-v0.10.28/node server.js
